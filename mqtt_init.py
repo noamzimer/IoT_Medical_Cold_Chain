14bf92f193ea27197a0638e7bba8e16dedbae527
noamzimer <zimernoam@gmail.com>
2026-04-07 13:36:55 +0300

    Add files via upload

--- mqtt_init.py
+++ mqtt_init.py
@@ -6,10 +6,11 @@ username = ""
 password = ""
 
 my_id = "5976397"
-comm_topic = "pr/home/" + my_id + "/"
-
+comm_topic = "pr/medical/" + my_id + "/"   
 topic_temp = comm_topic + "sts"
 topic_actuator = comm_topic + "act"
 topic_alarm = comm_topic + "alarm"
 
-TEMP_THRESHOLD = 28.0
+# Medical Cold Chain Thresholds (Vaccine Safe Range)
+MIN_TEMP = 2.0 
+MAX_TEMP = 8.0
